8abe4e96e7b80df11e9750c4a1b4cb30c6e854ec
linomp <lm2p@hotmail.es>
2023-04-22 21:06:27 +0200

    add python producer example

--- notes.md
+++ notes.md
@@ -7,3 +7,20 @@
 - kafka guarantees ordering at the partition level (not at the topic level)
 - messages with the same key will always go to the same partition
 - producers influence the throughput scalability
+    - based on key, msgs are assigned to partitions via hashing + modulo (default algo), or round-robin if no key is specified
+    - example; if the key is a category with only 2 options, you will only ever realize the value of 2 partitions!
+
+### CLI Producer/Consumer
+- To produce message with key:
+    ```
+    kafka-console-producer --bootstrap-server broker0:29092 --topic people --property "parse.key=true" --property "key.separator=|"
+    ``` 
+    example value:
+    ```
+    chief-scientist|{"name":"Martin Fowler", "title":"Chief Scientist"}
+    ```
+- To consume message and print key:
+    ```
+    kafka-console-consumer --bootstrap-server broker0:29092 --topic people --from-beggining --property "print.key=true"
+    ``` 
+
---
+++ python-exercises/peopleservice-basic-producer/main.py
@@ -0,0 +1,35 @@
+import logging
+import os
+
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from kafka import KafkaAdminClient
+from kafka.admin import NewTopic
+from kafka.errors import TopicAlreadyExistsError
+
+logger = logging.getLogger(__name__)
+
+load_dotenv(verbose=True)
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup_event():
+    client = KafkaAdminClient(
+        bootstrap_servers=os.environ.get("BOOTSTRAP_SERVERS"))
+    topic = NewTopic(name=os.environ.get("TOPICS_PEOPLE_BASIC_NAME"),
+                     num_partitions=int(os.environ.get(
+                         "TOPICS_PEOPLE_BASIC_PARTITIONS")),
+                     replication_factor=int(os.environ.get("TOPICS_PEOPLE_BASIC_REPLICAS")))
+    try:
+        client.create_topics([topic])
+    except TopicAlreadyExistsError:
+        logger.warning(f"Topic {topic} already exists")
+    finally:
+        client.close()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
